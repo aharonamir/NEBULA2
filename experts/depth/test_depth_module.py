a4d659f84c717161c89f20afa82a6654d271d40d
aharonamir <aharon.amir@gmail.com>
2022-02-07 16:54:57 +0000

    add test midas and working docker image

--- experts/depth/test_depth_module.py
+++ experts/depth/test_depth_module.py
@@ -1,7 +1,15 @@
 import sys
 import os
 
-#sys.path.append("../")
+import torch.cuda
+
+if torch.cuda.is_available():
+    print("CUDA is available :D")
+else:
+    print("CUDA isn't available :(")
+
+sys.path.append("/workspaces/NEBULA2")
+print(sys.path)
 from nebula_api.nebula_enrichment_api import NRE_API
 nre = NRE_API()
 db = nre.db
@@ -12,3 +20,53 @@ client = ArangoClient(hosts='http://ec2-18-158-123-0.eu-central-1.compute.amazon
 dbname = 'nebula_development'
 db_manual = client.db(dbname, username='nebula', password='nebula')
 print(db_manual)
+
+# midas v2 test
+import cv2
+import torch
+import urllib.request
+
+import matplotlib.pyplot as plt
+
+url, filename = ("https://github.com/pytorch/hub/raw/master/images/dog.jpg", "dog.jpg")
+urllib.request.urlretrieve(url, filename)
+# load model
+model_type = "DPT_Large"     # MiDaS v3 - Large     (highest accuracy, slowest inference speed)
+#model_type = "DPT_Hybrid"   # MiDaS v3 - Hybrid    (medium accuracy, medium inference speed)
+#model_type = "MiDaS_small"  # MiDaS v2.1 - Small   (lowest accuracy, highest inference speed)
+midas = torch.hub.load("intel-isl/MiDaS", model_type)
+
+# Move model to GPU if available
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+midas.to(device)
+midas.eval()
+
+# Load transforms to resize and normalize the image for large or small model
+midas_transforms = torch.hub.load("intel-isl/MiDaS", "transforms")
+if model_type == "DPT_Large" or model_type == "DPT_Hybrid":
+    transform = midas_transforms.dpt_transform
+else:
+    transform = midas_transforms.small_transform
+
+# Load image and apply transforms
+img = cv2.imread(filename)
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+input_batch = transform(img).to(device)
+
+# Predict and resize to original resolution
+with torch.no_grad():
+    prediction = midas(input_batch)
+
+    prediction = torch.nn.functional.interpolate(
+        prediction.unsqueeze(1),
+        size=img.shape[:2],
+        mode="bicubic",
+        align_corners=False,
+    ).squeeze()
+
+output = prediction.cpu().numpy()
+
+# show results
+plt.imshow(output)
+# plt.show()
